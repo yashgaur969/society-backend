6d86f7e682e2d46fa78fc8aa4bfb4b362ee05339
yash gaur <yash.gaur@hashedin.com>
2020-02-24 15:40:41 +0530

    login integrated with the frontend login page

--- app/models.py
+++ app/models.py
@@ -13,24 +13,28 @@ class AccessTokenTable(db.Model):
     access_token = db.Column(db.String(), primary_key=True)
 
 
-# class SocietyDetails(db.Model):
-#     society_type = db.Column(db.String())
-#     is_fenced = db.Column(db.String())
-#     is_guarded = db.Column(db.String())
-#
-#
-# class BuildingDetails(db.Model):
-#     number_of_floors = db.Column(db.Integer)
-#     number_of_flats = db.Column(db.Integer)
-#     total_flats = db.Column(db.Integer)
-#
-#
-# class ManagementContactInfo(db.Model):
-#     name = db.Column(db.String())
-#     contact = db.Column(db.String())
-#     email_id = db.Column(db.String())
-#
-#
+class SocietyDetails(db.Model):
+    society_id = db.Column(db.Integer, primary_key=True)
+    society_type = db.Column(db.String())
+    is_fenced = db.Column(db.String())
+    is_guarded = db.Column(db.String())
+
+
+class BuildingDetails(db.Model):
+    building_id = db.Column(db.Integer, primary_key=True)
+    society_id = db.Column(db.Integer, foreign_key=True)
+    number_of_floors = db.Column(db.Integer)
+    number_of_flats = db.Column(db.Integer)
+    total_flats = db.Column(db.Integer)
+
+
+class ManagementContactInfo(db.Model):
+    
+    name = db.Column(db.String())
+    contact = db.Column(db.String())
+    email_id = db.Column(db.String())
+
+
 # class AdditionalInfo(db.Model):
 #     has_lift = db.Column(db.String())
 #     lift_capacity = db.Column(db.Integer)
